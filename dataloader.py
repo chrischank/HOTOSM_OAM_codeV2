091b1ed4e7d2430625142cc7c0395977ec1d3438
chrischank <chrischank97@gmail.com>
2022-02-17 20:27:09 +0100

    basic training loop shape and dataloader pair

--- dataloader.py
+++ dataloader.py
@@ -9,7 +9,7 @@ import os
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
-from torchvision.io import read_image
+from torchvision.io import read_image, ImageReadMode
 
 class BuildingDataset(Dataset):
     def __init__(self, png_dir, lbl_dir, transform=None):
@@ -20,11 +20,14 @@ class BuildingDataset(Dataset):
     def __len__(self):
         return len(self.png_dir), len(self.lbl_dir)
 
-    def __getitem__(self):
+    def __getitem__(self, idx):
         img_path = os.path.join(self.png_dir)
         lbl_path = os.path.join(self.lbl_dir)
-        image = read_image(img_path)
-        label = read_image(lbl_path)
+
+        # Since gdal can only output format to UInt16
+        # We use the dataset building to format to UInt8
+        image = read_image(img_path[idx], mode = ImageReadMode.RGB)
+        label = read_image(lbl_path[idx], mode = ImageReadMode.GRAY)
         if self.transform: # What are these things?
             image = self.transform(image)
             label = self.transform(label)
