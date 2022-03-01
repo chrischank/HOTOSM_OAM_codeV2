17686758b111dd90b04b88d4cbf73b0cfd1203ee
chrischank <chrischank97@gmail.com>
2022-03-01 09:10:20 +0100

    -ot Byte

--- tiff2png.sh
+++ tiff2png.sh
@@ -4,7 +4,7 @@
 #This script parse tif into IMG & LBL PNG#
 #Maintainer: Christopher Chan            #
 #Date: 2022-02-17                        #
-#Version: 0.1.1                          #
+#Version: 0.1.2                          #
 ##########################################
 
 echo "This script parse .tif into IMG & LBL PNGs"
@@ -24,14 +24,14 @@ cd $IMG_dir
 
 for i in *.tif;
 do
-  gdal_translate -of PNG -ot UInt16 -b 4 $i $LBL_dir"/"$i".png";
+  gdal_translate -of PNG -ot Byte -b 4 $i $LBL_dir"/"$i".png";
 done
 
 echo "Extracting band 1 2 3 to IMG directory..."
 
 for i in *.tif;
 do
-  gdal_translate -of PNG -ot UInt16 -b 1 -b 2 -b 3 $i $IMG_dir"/"$i".png";
+  gdal_translate -of PNG -ot Byte -b 1 -b 2 -b 3 $i $IMG_dir"/"$i".png";
 done
 
 rm -rf *.tif
@@ -53,9 +53,3 @@ for i in *;
 do
   rename -v .tif "" $i;
 done
-
-echo "ATTENTION: PNG generated are of UInt16 formats, but the min max are 0 and 255. Therefore, the images might not show-up in normal photo editing software but only professional digital image softwarel. This is due to a technical limitation from gdal :(,
-
-... However, don't panic! The data is fine ...
-
-They will later be converted back to UInt8 when using the dataloader into pytorch and the neural network :)."
